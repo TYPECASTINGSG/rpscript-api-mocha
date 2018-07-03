c5ab0dcc59c788bad6e7081910c0cfd5f8acfcd4
James Chong <wei3hua2@gmail.com>
2018-07-03 11:57:15 +0800

    update interface

--- src/index.ts
+++ src/index.ts
@@ -16,20 +16,20 @@ export default class RpsMocha {
     if(ctx) ctx['mocha'] = new MochaContext;
   }
 
-  @rpsAction({defaultName:'testSuite'})
+  @rpsAction({verbName:'testSuite'})
   async testSuite (ctx:RpsContext,opts:{}, suitename:string) : Promise<Suite> {
 
     return RpsMocha.util.createSuite(ctx['mocha'], {}, suitename);
   }
 
-  @rpsAction({defaultName:'testCase'})
+  @rpsAction({verbName:'testCase'})
   async testCase (ctx:RpsContext, opts:TestCaseOpts,
     testname:string, fn:()=>void) : Promise<Test> {
 
     return RpsMocha.util.createTestCase(ctx['mocha'],opts,testname,fn);
   }
 
-  @rpsActionSkipErrHandling({defaultName:'expect'})
+  @rpsActionSkipErrHandling({verbName:'expect'})
   async expect (ctx:RpsContext, opts:TestCaseOpts,
     expect:any, actual:any, ...chains:string[]) : Promise<void> {
   
@@ -38,7 +38,7 @@ export default class RpsMocha {
     return Promise.resolve();
   }
 
-  @rpsAction({defaultName:'testReport'})
+  @rpsAction({verbName:'testReport'})
   async testReport (ctx:RpsContext, opts:any) : Promise<Runner> {
     
     return RpsMocha.util.runTest(ctx['mocha'],opts);
